e3287b975391a1a7e8cdda2ff59a1a1ef9be87ab
Luka <odak.skywalker@gmail.com>
2020-04-06 12:16:14 +0200

    button adds editable text

--- app/src/main/java/com/example/playing/MainActivity.kt
+++ app/src/main/java/com/example/playing/MainActivity.kt
@@ -10,10 +10,7 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private var userInput: EditText? = null
-    private var button: Button? = null
     private var textView: TextView? = null
-
     private var numTimesClicked = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +19,21 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.note_layout)
         setContentView(R.layout.new_layout)
 
-        userInput = findViewById<EditText>(R.id.editText)
-        button = findViewById<Button>(R.id.button)
+        val userInput: EditText = findViewById<EditText>(R.id.editText)
+        val button: Button = findViewById<Button>(R.id.button)
 
         textView = findViewById<TextView>(R.id.textView)
         textView?.text = ""
         textView?.movementMethod = ScrollingMovementMethod()
 
+        userInput.setText("")
 
-        button?.setOnClickListener(object : View.OnClickListener{
+
+        button.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-                numTimesClicked += 1
-                textView?.append("The button got tapped $numTimesClicked time")
-                if(numTimesClicked != 1){
-                    textView?.append("s\n")
-                } else{
-                    textView?.append("\n")
-                }
-
-
+                textView?.append(userInput.text)
+                textView?.append("\n")
+                userInput.setText("")
             }
         })
 
