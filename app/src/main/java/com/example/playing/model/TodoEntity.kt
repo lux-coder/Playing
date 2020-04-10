bdcffa9d9167046728a6d5e053c58606fa55a9ac
Luka <odak.skywalker@gmail.com>
2020-04-10 10:44:23 +0200

    diff database implementation

---
+++ app/src/main/java/com/example/playing/model/TodoEntity.kt
@@ -0,0 +1,19 @@
+package com.example.playing.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.sql.RowId
+
+@Entity(tableName = "todo_items")
+data class TodoEntity(
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+
+    @ColumnInfo(name = "title")
+    var title: String,
+
+    @ColumnInfo(name = "content")
+    var content: String
+)
