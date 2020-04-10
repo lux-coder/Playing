68b90a20c4a1e5fe1a467d197f48c9ff0ae6a546
Luka <odak.skywalker@gmail.com>
2020-04-10 11:29:05 +0200

    deleted unused classes

--- app/src/main/java/com/example/playing/model/INoteRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.playing.model
-
-interface INoteRepository {
-
-    //suspend fun getNoteById(noteId: String): Result<Exception, Note>
-
-}
--- app/src/main/java/com/example/playing/model/Note.kt
+++
@@ -1,9 +0,0 @@
-package com.example.playing.model
-
-data class Note(
-    val creationDate: String,
-    val contents: String,
-    val upVotes: Int,
-    val imageUrl: String,
-    val creatorId: Int
-)
